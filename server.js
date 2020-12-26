d64881f4c264206d927e433ed76e7b4d6f8c5542
dummytarget <kdkchy@gmail.com>
2020-12-26 11:15:44 +0700

    last commit, v1.0

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const app = express()
 const articleRouter = require('./routes/articles')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost/blog',
 {
-    useNewUrlParser : true, useUnifiedTopology: true
+    useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true
 })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 
 
 app.get('/', async (req,res)=> {
-    const articles = await Article.find() 
+    const articles = await Article.find().sort({createdAt : 'desc'})
     res.render('articles/index', {articles:articles})
 })
 
